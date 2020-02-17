947343ad1e9ebbfdd9bd29c4c13e573583266618
Chris Proctor <chris.proctor@gmail.com>
2020-02-17 08:02:32 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qualitative-coding",
-    version="0.1.0",
+    version="0.1.1",
     author="Chris Proctor",
     author_email="pypi.org@accounts.chrisproctor.net",
     description="Qualitative coding tools for computer scientists",
