947716acbea09810a9ef846fee1613c4695eba9b
Chris Proctor <chris@chrisproctor.net>
2024-02-11 20:55:49 -0500

    Add docstring to coders taslk

--- qualitative_coding/cli/coders.py
+++ qualitative_coding/cli/coders.py
@@ -6,6 +6,7 @@ from qualitative_coding.corpus import QCCorpus
 @click.command()
 @click.option("-s", "--settings", default="settings.yaml", help="Settings file")
 def coders(settings):
+    "List all coders"
     s = yaml.safe_load(Path(settings).read_text())
     corpus = QCCorpus(s)
     with corpus.session():
