15a2e8e9b9fa3e3e2d5503ef77478bb6f55127a6
Chris Proctor <chris@chrisproctor.net>
2024-02-19 14:54:24 -0500

    Tests for find

---
+++ qualitative_coding/tests/test_find.py
@@ -0,0 +1,19 @@
+from tests.fixtures import QCTestCase
+
+class TestFind(QCTestCase):
+    def setUp(self):
+        super().setUp()
+        self.run_in_testpath("qc import macbeth.txt --importer verbatim")
+        self.set_mock_editor()
+        self.run_in_testpath("qc code chris")
+        self.run_in_testpath("qc codebook")
+
+    def test_find_shows_codes(self):
+        result = self.run_in_testpath("qc find one")
+        self.assertEqual(len(result.stdout.splitlines()), 8)
+
+    def test_find_respects_context_window(self):
+        result = self.run_in_testpath("qc find one -C 5")
+        self.assertEqual(len(result.stdout.splitlines()), 11)
+
+
