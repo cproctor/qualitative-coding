19fe2847c2d8c4bb5bdb5f98bddcb358de142529
Chris Proctor <chris@chrisproctor.net>
2024-03-13 21:26:18 -0400

    Add tests for stats by coder and by document

--- qualitative_coding/tests/test_codes_stats.py
+++ qualitative_coding/tests/test_codes_stats.py
@@ -9,10 +9,26 @@ class TestStats(QCTestCase):
         table = self.read_stats_tsv(result.stdout)
         self.assertEqual(table['line']['Count'], 2)
 
-    def test_stats_distinct_shows_distinct_coders(self):
+    def test_stats_distinct_shows_totals_by_coder(self):
         self.run_in_testpath("qc corpus import macbeth.txt --importer verbatim")
         self.set_mock_editor()
         self.run_in_testpath("qc code chris")
         self.run_in_testpath("qc code haley")
-        result = self.run_in_testpath("qc codes stats --distinct --format tsv")
+        result = self.run_in_testpath("qc codes stats --by-coder --format tsv")
+        table = self.read_stats_tsv(result.stdout)
+
+    def test_stats_distinct_shows_totals_by_document(self):
+        self.run_in_testpath("qc corpus import macbeth.txt --importer verbatim")
+        self.set_mock_editor()
+        self.run_in_testpath("qc code chris")
+        self.run_in_testpath("qc code haley")
+        result = self.run_in_testpath("qc codes stats --by-document --format tsv")
+        table = self.read_stats_tsv(result.stdout)
+
+    def test_stats_distinct_shows_coder_document_pivot_table(self):
+        self.run_in_testpath("qc corpus import macbeth.txt --importer verbatim")
+        self.set_mock_editor()
+        self.run_in_testpath("qc code chris")
+        self.run_in_testpath("qc code haley")
+        result = self.run_in_testpath("qc codes stats --by-document --by-coder --format tsv")
         table = self.read_stats_tsv(result.stdout)
