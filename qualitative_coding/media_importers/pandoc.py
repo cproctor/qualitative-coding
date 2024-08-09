6480e8b151118646a4c4b9883fad1c41dc1cbe1d
Chris Proctor <chris@chrisproctor.net>
2024-08-09 17:23:13 -0400

    Add check for pandoc with helpful error message

--- qualitative_coding/media_importers/pandoc.py
+++ qualitative_coding/media_importers/pandoc.py
@@ -1,8 +1,16 @@
-from subprocess import run
+from subprocess import run, CalledProcessError
 from qualitative_coding.media_importers.base import BaseMediaImporter
+from qualitative_coding.exceptions import QCError
 
 class PandocImporter(BaseMediaImporter):
     def import_media(self, input_filename, output_filename):
+        self.check_for_pandoc()
         cmd = f'pandoc -i "{input_filename}" -o "{output_filename}" --to plain --columns 80'
         run(cmd, shell=True, check=True)
+
+    def check_for_pandoc(self):
+        try:
+            run("which pandoc", check=True)
+        except CalledProcessError:
+            raise QCError("pandoc is required but was not found. Please install pandoc.")
 
