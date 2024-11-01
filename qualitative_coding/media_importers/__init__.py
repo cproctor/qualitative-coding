57d833f1f33cba7e3d1d551616611015e240d36d
Chris Proctor <chris@chrisproctor.net>
2024-11-01 10:02:47 -0400

    Add VTT media importer

--- qualitative_coding/media_importers/__init__.py
+++ qualitative_coding/media_importers/__init__.py
@@ -1,8 +1,10 @@
 from qualitative_coding.exceptions import InvalidParameter
 from qualitative_coding.media_importers.pandoc import PandocImporter
 from qualitative_coding.media_importers.verbatim import VerbatimImporter
+from qualitative_coding.media_importers.vtt import VTTImporter
 
 media_importers = {
     "pandoc": PandocImporter,
     "verbatim": VerbatimImporter,
+    "vtt": VTTImporter,
 }
---
+++ qualitative_coding/media_importers/vtt.py
@@ -0,0 +1,25 @@
+from qualitative_coding.media_importers.base import BaseMediaImporter
+from textwrap import fill
+import webvtt
+
+class VTTImporter(BaseMediaImporter):
+    """Imports a VTT transcript file, stripping out timestamps and collapsing 
+    adjacent talk turns from the same speaker.
+    """
+    def import_media(self, input_filename, output_filename):
+        turns = []
+        current_speaker = None
+        current_speech = ""
+        for caption in webvtt.read(input_filename):
+            speaker, speech = caption.text.split(':', 1)
+            if speaker == current_speaker: 
+                current_speech += speech
+            else:
+                if current_speech: 
+                    turns.append({'speaker': current_speaker, 'speech': current_speech})
+                current_speaker = speaker
+                current_speech = speech
+        turns.append({'speaker': current_speaker, 'speech': current_speech})
+        with open(output_filename, 'w') as fh:
+            for turn in turns:
+                fh.write('\n' + fill(turn['speaker'] + ': ' + turn['speech'], width=80))
